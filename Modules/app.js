71e315bbde5758c4e8297e8816a6db8e750ca695
Charles Chen <charles.chen@parrowtech.com>
2015-07-17 14:31:28 +0800

    added amap lib

--- Modules/app.js
+++ Modules/app.js
@@ -479,6 +479,7 @@ myChart.on(ecConfig.EVENT.MAP_SELECTED, function (param){
     }
     else {
       console.log('选择的城市： ' + param.target);
+      getCurrentPosition();
     }
     console.log();
     option.series[0].mapType = mt;
@@ -495,5 +496,60 @@ myChart.on(ecConfig.EVENT.MAP_SELECTED, function (param){
   };
 
 
+
+  function getCurrentPosition () { //调用浏览器定位服务
+    var mapObj=new AMap.Map("map",{
+    view: new AMap.View2D({//创建地图二维视口
+    zoom:14, //设置地图缩放级别
+    rotation:0 //设置地图旋转角度
+   }),
+   lang:"zh_cn"//设置地图语言类型，默认：中文简体
+  });//创建地图实例
+    mapObj.plugin('AMap.Geolocation', function () {
+        geolocation = new AMap.Geolocation({
+            enableHighAccuracy: true,//是否使用高精度定位，默认:true
+            timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+            maximumAge: 0,           //定位结果缓存0毫秒，默认：0
+            convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+            showButton: true,        //显示定位按钮，默认：true
+            buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
+            buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+            showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
+            showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
+            panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
+            zoomToAccuracy:true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        });
+        mapObj.addControl(geolocation);
+      //  AMap.event.addListener(geolocation, 'complete', onComplete);//返回定位信息
+        //AMap.event.addListener(geolocation, 'error', onError);      //返回定位出错信息
+    });
+};
+
+  var getAMap = function(cityName){
+    var position=new AMap.LngLat(116.397428,39.90923);
+    var mapObj=new AMap.Map("map",{
+    view: new AMap.View2D({//创建地图二维视口
+    center:position,//创建中心点坐标
+    zoom:14, //设置地图缩放级别
+    rotation:0 //设置地图旋转角度
+   }),
+   lang:"zh_cn"//设置地图语言类型，默认：中文简体
+  });//创建地图实例
+
+var marker = new AMap.Marker({ //创建自定义点标注
+  map:mapObj,
+  position: new AMap.LngLat(116.406326, 39.903942),
+  offset: new AMap.Pixel(-10,-34)
+});
+marker.setMap(mapObj);
+/*
+var marker2 = new AMap.Marker({ //创建自定义点标注
+  map:mapObj,
+  position: new AMap.LngLat(116.407326, 39.907942),
+  offset: new AMap.Pixel(-10,-34)
+});
+marker2.setMap(mapObj);
+*/
+};
 
 });
